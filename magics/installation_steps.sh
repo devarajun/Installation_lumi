9d944614f3011a53dd14535b1d83e27f1803c79f
Devaraju N <devarajun@gmail.com>
2024-03-20 10:29:36 +0200

    Update installation_steps.sh

--- magics/installation_steps.sh
+++ magics/installation_steps.sh
@@ -1,2 +1,4 @@
+#!/bins/bash
+#use conda-container on LUMI to install Magics ( https://confluence.ecmwf.int/display/MAGP/Magics)
 module load lumi-container-wrapper
 conda-containerize new --prefix=/project/project_465000454/devaraju/softwares/Magics  env.yml
